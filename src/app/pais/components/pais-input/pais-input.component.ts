5b7f0650d5d77e979f5de9b35007aafffeb8c842
nef <neftalitorres@itesm.mx>
2021-07-15 16:28:11 -0500

    video 107

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+  ]
+})
+export class PaisInputComponent  {
+termino : string ='';
+
+
+buscar(){
+  console.log("Hola mundo");
+  console.log(this.termino);
+}
+
+
+
+}
