c37f10156a9ea7af0873c53bd620f68647c448ab
nef <neftalitorres@itesm.mx>
2021-07-15 11:12:14 -0500

    video 101

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,10 +10,12 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   exports:[
+
     SidebarComponent
   ] ,
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
