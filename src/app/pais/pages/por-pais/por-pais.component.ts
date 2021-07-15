d53c45f432213a2e2487fbf4f0f32591fcda68d7
nef <neftalitorres@itesm.mx>
2021-07-15 15:55:11 -0500

    video 106

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -11,6 +12,7 @@ export class PorPaisComponent {
 
   termino : string ='';
   hayError : boolean = false;
+  paises : Country[] = [];
   constructor(private paisServices:PaisService) { }
 
   buscar(){
@@ -19,9 +21,10 @@ export class PorPaisComponent {
     this.paisServices.buscarPais(this.termino)
     .subscribe(paises=>{    
       console.log(paises);
-    
+      this.paises = paises;
     },(err)=>{
-    this.hayError=true;
+      this.hayError=true;
+      this.paises =[];
     });
   }
   
