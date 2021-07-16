433c58ea4f6815266ec6f6d2571a248eedaadd05
nef <neftalitorres@itesm.mx>
2021-07-16 18:15:30 -0500

    video 112

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap} from 'rxjs/operators';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -16,15 +17,24 @@ export class VerPaisComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-    this.activatedRaute.params
-    .subscribe(({id}) =>{
-      console.log(id);
 
-      this.paisServicio.getPaisPorAlpha(id)
-      .subscribe(pais=>{
-        console.log("El pais es ",pais);
-      })
+    this.activatedRaute.params
+    .pipe(
+      switchMap(({id})=>this.paisServicio.getPaisPorAlpha(id))
+    )
+    .subscribe(resp=>{
+      console.log(resp);
     })
+
+    // this.activatedRaute.params
+    // .subscribe(({id}) =>{
+    //   console.log(id);
+
+    //   this.paisServicio.getPaisPorAlpha(id)
+    //   .subscribe(pais=>{
+    //     console.log("El pais es ",pais);
+    //   })
+    // })
   }
 
 }
