818a25f6fc614ecf7f8d7a4d66e39d2b3018e200
nef <neftalitorres@itesm.mx>
2021-07-16 17:36:47 -0500

    video 110

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -6,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
 
-  constructor() { }
+  termino : string ='';
+  hayError : boolean = false;
+  paises : Country[] = [];
+  constructor(private paisServices:PaisService) { }
+  
+  buscar(termino : string){
+    this.hayError=false;
+    this.termino = termino;
+    
+    
+    this.paisServices.buscarCapital(termino)
+    .subscribe(paises=>{    
+      console.log(paises);
+      this.paises = paises;
 
-  ngOnInit(): void {
+    },(err)=>{
+      this.hayError=true;
+      this.paises =[];
+    });
   }
 
+
+  
 }
