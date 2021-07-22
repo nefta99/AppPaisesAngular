bef7ea881e485b2239a088917175879340dcd86e
nef <neftalitorres@itesm.mx>
2021-07-22 18:07:55 -0500

    video 123

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -10,16 +11,24 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
 
   private apiUrl :string  ='https://restcountries.eu/rest/v2';
+
+
   constructor(private http: HttpClient) { }
+
+
+  get httpParams(){
+    return new HttpParams().set( 'fields', 'name;capital;alpha2Code;flag;population');
+
+  }
 
   buscarPais(termino: string): Observable <Country[]>{
     const url =`${ this.apiUrl }/name/${ termino }`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{params : this.httpParams});
      
   }
   buscarCapital(termino: string): Observable <Country[]>{
     const url =`${ this.apiUrl }/capital/${ termino }`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{params: this.httpParams});
      
   }
   getPaisPorAlpha(id: string): Observable <Country>{
@@ -30,7 +39,14 @@ export class PaisService {
 
 
   buscarRegion(region : string ): Observable<Country[]>{
+    const Params = new HttpParams()
+      .set( 'fields', 'name;capital;alpha2Code;flag;population');
+
+
     const url =`${ this.apiUrl }/region/${ region }`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{ params: Params})
+    .pipe(
+      tap(console.log)
+    )
   }
 }
